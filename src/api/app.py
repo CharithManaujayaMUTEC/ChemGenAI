a8bf8b61c4ea3a29018f3bb8c2e9c4411315ce94
G.C.M. <charithmanujaya1@gmail.com>
2026-05-31 02:51:38 +0530

    app updated

--- src/api/app.py
+++ src/api/app.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+from src.api.model_service import generate_smiles
 
 app = FastAPI(
     title="ChemGenAI API",
     version="1.0.0"
 )
+
+
+class GenerateRequest(BaseModel):
+    prompt: str
 
 
 @app.get("/")
@@ -19,8 +26,13 @@ def health():
         "status": "healthy"
     }
 
+
 @app.post("/generate")
 def generate(request: GenerateRequest):
+
+    smiles = generate_smiles()
+
     return {
-        "generated_smiles": "CCO"
+        "prompt": request.prompt,
+        "generated_smiles": smiles
     }
